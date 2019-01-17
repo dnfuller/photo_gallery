57f1d521f7f8e9764186c4acb67a34e34b71e51d
dnfuller <dnfuller92@gmail.com>
2019-01-17 13:27:04 -0700

    added html, css, js, and updated internal links for gallery photos

---
+++ app.js
@@ -0,0 +1,10 @@
+$(function() {
+  var icons = {
+    header: "ui-icon-circle-arrow-e",
+    activeHeader: "ui-icon-circle-arrow-s"
+  };
+  $("#accordion").accordion({
+    icons: icons,
+    collapsible: true
+  });
+});
